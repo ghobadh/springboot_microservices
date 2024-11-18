a58feafb60079a9f1847dbc5b19546ed9ac347b7
ghobadh <ghobadh@gmail.com>
2024-11-18 10:27:42 -0500

    Initial commit

---
+++ src/test/java/ca/gforcesoftware/microservice/depratmentservice/DepratmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.microservice.depratmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepratmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
