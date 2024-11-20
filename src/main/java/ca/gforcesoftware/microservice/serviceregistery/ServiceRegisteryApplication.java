23d09d1688766316cf5565b2255886ccf7e6703d
ghobadh <ghobadh@gmail.com>
2024-11-19 22:11:46 -0500

    Eureka and load balancer are added

--- src/main/java/ca/gforcesoftware/microservice/serviceregistery/ServiceRegisteryApplication.java
+++ src/main/java/ca/gforcesoftware/microservice/serviceregistery/ServiceRegisteryApplication.java
@@ -2,8 +2,10 @@ package ca.gforcesoftware.microservice.serviceregistery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegisteryApplication {
 
 	public static void main(String[] args) {
