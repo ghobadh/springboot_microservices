a65403bb1f7e5edeb18c3ef3da670e2fe02124b5
ghobadh <ghobadh@gmail.com>
2024-11-19 19:47:15 -0500

    Webclient changed to Spring Cloud Feign  in EmployeeService

---
+++ src/test/java/ca/gforcesoftware/microservice/serviceregistery/ServiceRegisteryApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.gforcesoftware.microservice.serviceregistery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegisteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
