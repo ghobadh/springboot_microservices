cbe883af723621b53a4bd8baa960455d8b3c0e44
Gavin Hashemi <ghobadh@gmail.com>
2024-11-21 22:47:24 -0500

    RabbitMQ for automation update has been added

---
+++ src/main/java/ca/gforcesoftware/microservice/employeeservice/controller/MessageController.java
@@ -0,0 +1,25 @@
+package ca.gforcesoftware.microservice.employeeservice.controller;
+
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author gavinhashemi on 2024-11-21
+ */
+@RestController
+@RefreshScope  // This annotation push the spring to load this class file
+public class MessageController {
+
+    @Value("${spring.boot.message}")
+    private String message;
+
+
+    @GetMapping("/users/message")
+    public String message() {
+        return message;
+    }
+}
